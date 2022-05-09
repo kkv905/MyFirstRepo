550e9ed553e61e4e8b206d3a3bfb6fa6d8b690a6
Kirill Koptelov <kkv905@outlook.com>
2022-05-10 00:25:30 +0300

    Добавил новые файлы

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+const skills = ['Dev', 'DevOps', 'Testing', 'Calc'];
+for (const skill of skills) {
+    console.log(skill.toLowerCase());
+}
+console.log('!!!!!!');
+const res = skills
+    .filter(s => s !== 'DevOps')
+    .map(s => s + '! ')
+    .reduce((a, b) => a + b);
+console.log(res);
